572f8b3cabad453abc38f8b9c6463755f89f798e
hardtoguess <callback.me@protonmail.com>
2020-02-24 21:39:09 +0300

    ..

--- src/App.vue
+++ src/App.vue
@@ -18,16 +18,16 @@
                     player2: { id: "2", name: "Team2", winner: false, points: 0 }
                 },
                 {
-                    player1: { id: "3", name: "Competitor 3", winner: false },
-                    player2: { id: "4", name: "Competitor 4", winner: true }
+                    player1: { id: "3", name: "Competitor 3", winner: false, points: 1 },
+                    player2: { id: "4", name: "Competitor 4", winner: true, points: 2 }
                 },
                 {
-                    player1: { id: "5", name: "Competitor 5", winner: true },
-                    player2: { id: "6", name: "Competitor 6", winner: false }
+                    player1: { id: "5", name: "Competitor 5", winner: true, points: 1  },
+                    player2: { id: "6", name: "Competitor 6", winner: false, points: 1 }
                 },
                 {
-                    player1: { id: "7", name: "Competitor 7", winner: false },
-                    player2: { id: "8", name: "Competitor 8", winner: true }
+                    player1: { id: "7", name: "Competitor 7", winner: false, points: 1 },
+                    player2: { id: "8", name: "Competitor 8", winner: true, points: 1 }
                 }
             ]
         },
@@ -35,8 +35,8 @@
         {
             games: [
                 {
-                    player1: { id: "1", name: "Competitor 1", winner: false },
-                    player2: { id: "4", name: "Competitor 4", winner: true }
+                    player1: { id: "1", name: "Competitor 1", winner: false, points: 1 },
+                    player2: { id: "4", name: "Competitor 4", winner: true, points: 1 }
                 },
                 {
                     player1: { id: "5", name: "Competitor 5", winner: false },
